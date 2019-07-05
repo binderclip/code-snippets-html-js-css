6b88c74d3721f25f6464ca62ac7a7614388f93c4
zhoubokun <zhoubokun@bytedance.com>
2019-07-05 23:09:42 +0800

    promise
    Change-Id: I3686e78913473e4a0878beb2af3e74d0b3dd99fb

--- js/date/date.js
+++ js/date/date.js
@@ -15,9 +15,12 @@ var date4 = new Date('1995-12-17 04:24:00');
 console.log(date3 > date4);
 
 var ts = Date.now();
-console.log(ts);
+console.log(ts); // 1562338724879
+var ts2 = + new Date();
+console.log(ts2); // 1562338778067
 var now = new Date();
-console.log(now);
+console.log(now); // 2019-07-05T14:58:44.880Z
 
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Date
 // https://www.w3schools.com/js/js_dates.asp
+// https://stackoverflow.com/questions/221294/how-do-you-get-a-timestamp-in-javascript
---
+++ js/promise/readme.md
@@ -0,0 +1,7 @@
+# promise
+
+## refs
+
+- [使用 Promises - JavaScript | MDN](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Using_promises)
+- [Promise 对象 - ECMAScript 6入门](http://es6.ruanyifeng.com/#docs/promise)
+- [Promise - 廖雪峰的官方网站](https://www.liaoxuefeng.com/wiki/1022910821149312/1023024413276544)
