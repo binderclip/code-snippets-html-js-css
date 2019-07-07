dd2a0e6f9b3b052aad3dd03293ef54bc694c4209
zhoubokun <zhoubokun@bytedance.com>
2019-07-07 20:30:51 +0800

    string array
    Change-Id: I2f21bfb4f5b1617d7e6e7a9bc7edfde73aed2826

---
+++ js/array/array.js
@@ -0,0 +1,5 @@
+console.log("== array index ==")
+var a = [1, 2, 3]
+console.log(a)
+console.log(a[0])
+console.log(a[a.length-1]) // last one
--- js/string/string.js
+++ js/string/string.js
@@ -1,5 +1,9 @@
+console.log("== string and ==")
 var s1 = "foo"
 var s2 = "bar"
 var s3 = s1 + s2
-
 console.log(s3)
+
+console.log("== string split ==")
+var s4 = "foo-bar-baz-"
+console.log(s4.split("-"))
