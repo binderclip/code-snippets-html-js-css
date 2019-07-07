ea5885deed0efa8ca61b0726104e3a84bb59306c
zhoubokun <zhoubokun@bytedance.com>
2019-07-07 23:50:29 +0800

    uuid
    Change-Id: I7767787bc141bb3a41b513bb7bedb8fffdb4cfd8

--- js/uuid/uuid.js
+++ js/uuid/uuid.js
@@ -1,3 +1,12 @@
-console.log("== uuid v4 ==")
-const uuidv4 = require('uuid/v4');
-console.log(uuidv4()); // '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+console.log("== uuid v4 (uuid lib) ==")
+const uuid_v4 = require('uuid/v4');
+console.log(uuid_v4()); // '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+console.log("== uuid v4 (random) ==")
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
+console.log(uuidv4())
