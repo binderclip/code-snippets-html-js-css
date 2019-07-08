77919e2487a76052eced234e9d9f17a174e9ad0a
zhoubokun <zhoubokun@bytedance.com>
2019-07-09 00:58:37 +0800

    array
    Change-Id: I1d56659f9b9fe161a4061d0acf36b6e2ad122422

--- js/array/array.js
+++ js/array/array.js
@@ -3,3 +3,10 @@ var a = [1, 2, 3]
 console.log(a)
 console.log(a[0])
 console.log(a[a.length-1]) // last one
+
+console.log("== array splice ==")
+var a1 = ["a", "b", "c"]
+a1.splice(1, 0, "x")
+a1.splice(1, 0, "y")
+a1.splice(5, 0, "z")
+console.log(a1)
