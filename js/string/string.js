d68efec3254fcced09409cf7f71b39bfda0a8cf0
zhoubokun <zhoubokun@bytedance.com>
2019-07-09 00:16:19 +0800

    substring
    Change-Id: If7411b29ddf2e25e6f38982a0601d76c3777c535

--- js/string/string.js
+++ js/string/string.js
@@ -7,3 +7,9 @@ console.log(s3)
 console.log("== string split ==")
 var s4 = "foo-bar-baz-"
 console.log(s4.split("-"))
+
+console.log("== sub string ==")
+var s5 = "Hello, 世界！"
+console.log(s5.length)
+console.log(s5.substring(7))
+console.log(s5.substring(7, 8))
